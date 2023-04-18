4f0d45a3ebde90240ae4c6f58eeb9b2b1ad34453
MumaBrian <brianmuma5@gmail.com>
2023-04-17 22:50:18 -0600

    installed express, nodemon

--- build/index.js
+++ build/index.js
@@ -1,1 +1,15 @@
-console.log('hello world');
+const express = require("express");
+const app = express();
+const logger = require("morgan");
+const port = 8000;
+app.use(logger("dev"));
+app.get("/", function (req, res) {
+    res.send("Hello World!");
+});
+// About page route
+app.get("/about", function (req, res) {
+    res.send("About this wiki");
+});
+app.listen(port, function () {
+    console.log(`Example app listening on port ${port}!`);
+});
