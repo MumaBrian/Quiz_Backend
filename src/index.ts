346dab8bbd33980a843dffcb07fceef8fb5dae4a
MumaBrian <brianmuma5@gmail.com>
2023-04-25 03:59:26 -0600

    today

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-const express = require("express");
+import express from "express"
 const app = express();
-const logger = require("morgan");
+// const logger = require("morgan");
 const port = 8000;
 
-app.use(logger("dev"));
+// app.use(logger("dev"));
 
 app.get("/", function (req, res) {
     res.send("Hello New World!");
@@ -17,3 +17,23 @@ app.get("/about", function (req, res) {
 app.listen(port, function () {
     console.log(`Example app listening on port ${port}!`);
 });
+
+class Server{
+    public app: express.Application;
+    constructor() {
+        this.app = express();// same as line 2
+        this.config();
+    }
+    public config() {
+        this.app.set("port", port);
+        app.get("/", function (req, res) {
+        res.send("Hello New World!");
+});
+    }
+    public start:void {
+        app.listen(port, function () {
+        console.log(`Example app listening on port ${port}!`);
+});
+    }
+    
+}
